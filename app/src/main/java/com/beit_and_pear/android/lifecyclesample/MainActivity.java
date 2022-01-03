2b8aed211f691360bd09e7a4092cb8e11aa9f9a4
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2022-01-03 19:25:04 +0900

    Initial commit

---
+++ app/src/main/java/com/beit_and_pear/android/lifecyclesample/MainActivity.java
@@ -0,0 +1,48 @@
+package com.beit_and_pear.android.lifecyclesample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i("LifeCycleSample", "Main onCreate() called");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart() {
+        Log.i("LifeCycleSample", "Main onStart() called");
+        super.onStart();
+    }
+
+    @Override
+    public void onRestart() {
+        Log.i("LifeCycleSample", "Main onRestart() called");
+        super.onRestart();
+    }
+    @Override
+    public void onResume() {
+        Log.i("LifeCycleSample", "Main onResume() called");
+        super.onResume();
+    }
+    @Override
+    public void onPause() {
+        Log.i("LifeCycleSample", "Main onPause() called");
+        super.onPause();
+    }
+    @Override
+    public void onStop() {
+        Log.i("LifeCycleSample", "Main onStop() called");
+        super.onStop();
+    }
+    @Override
+    public void onDestroy() {
+        Log.i("LifeCycleSample", "Main onDestroy() called");
+        super.onDestroy();
+    }
+}
